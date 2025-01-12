c96cdac85d1e52be088ad4a2d5ace2d49afaeb2e
Sarfraz Alam <sarfrazk7858@gmail.com>
2025-01-12 12:36:18 +0530

    done stream

--- streams/write-many/writeMany.js
+++ streams/write-many/writeMany.js
@@ -18,8 +18,6 @@
 
 
 
-
-
 // const fs = require('node:fs');
 
 // (async () => {
@@ -40,26 +38,6 @@
 // CPU Usage: 100% (one once)
 // Memory Usage: 50MB
 
-// const fs = require('node:fs/promises');
-
-// (async () => {
-//     console.time("writeMany")
-//     const fileHandler = await fs.open("text.txt","w");
-
-//     const stream = fileHandler.createWriteStream()
-
-//     for(let i=0; i<1000000; i++){
-//         const buff = Buffer.from(` ${i} `, "utf-8");
-//         stream.write(buff)
-//     }
-
-//     console.timeEnd("writeMany")
-
-// })()
-
-
-
-
 const fs = require('node:fs/promises');
 
 (async () => {
@@ -68,57 +46,77 @@ const fs = require('node:fs/promises');
 
     const stream = fileHandler.createWriteStream()
 
-    console.log(stream.writableHighWaterMark)
-    console.log(stream.writableLength)
-
-    // const buff = Buffer.alloc(16383, 10)
-    // console.log(buff)
-    // console.log(stream.write(buff))
-    // console.log(stream.write(Buffer.alloc(1, "a")))
-    // console.log(stream.write(Buffer.alloc(1,"a")))
-    // console.log(stream.write(Buffer.alloc(1,"a")))
-
-
-    // this will execute when allocation is full
-    // stream.on("drain", () => {
-    //     // console.log(stream.write(Buffer.alloc(1, "a")))
-    //     // console.log(stream.writableLength);
-
-    //     console.log("we are not safe to write more!")
-    // })
-
-    // const buff = Buffer.from("string")
-    // console.log(buff)
-    // console.log(stream.writableLength)
-
-
-    let i = 0;
-
-    const writeMany = () => {
-        while (i < 1000000) {
-            const buff = Buffer.from(` ${i} `, "utf-8");
-
-            // this is our last write
-            if (i === 999999) {
-                return stream.end(buff)
-            }
-
-            // if stream.write returns false, stop the loop  
-            if (!stream.write(buff)) break;
-
-            i++;
-        }
+    for (let i = 0; i < 1000000; i++) {
+        const buff = Buffer.from(` ${i} `, "utf-8");
+        stream.write(buff)
     }
-    writeMany()
 
-    // resume our loop once our stream's internal buffer is empty
-    stream.on("drain", () => {
-        writeMany()
-    })
-
-    stream.on("finish", ()=>{
-        console.timeEnd("writeMany")
-        fileHandler.close()
-    })
+    console.timeEnd("writeMany")
 
 })()
+
+
+
+
+// const fs = require('node:fs/promises');
+
+// (async () => {
+//     console.time("writeMany")
+//     const fileHandler = await fs.open("text.txt", "w");
+
+//     const stream = fileHandler.createWriteStream()
+
+//     console.log(stream.writableHighWaterMark)
+//     console.log(stream.writableLength)
+
+//     // const buff = Buffer.alloc(16383, 10)
+//     // console.log(buff)
+//     // console.log(stream.write(buff))
+//     // console.log(stream.write(Buffer.alloc(1, "a")))
+//     // console.log(stream.write(Buffer.alloc(1,"a")))
+//     // console.log(stream.write(Buffer.alloc(1,"a")))
+
+
+//     // this will execute when allocation is full
+//     // stream.on("drain", () => {
+//     //     // console.log(stream.write(Buffer.alloc(1, "a")))
+//     //     // console.log(stream.writableLength);
+
+//     //     console.log("we are not safe to write more!")
+//     // })
+
+//     // const buff = Buffer.from("string")
+//     // console.log(buff)
+//     // console.log(stream.writableLength)
+
+
+//     let i = 0;
+
+//     const writeMany = () => {
+//         while (i < 1000000) {
+//             const buff = Buffer.from(` ${i} `, "utf-8");
+
+//             // this is our last write
+//             if (i === 999999) {
+//                 return stream.end(buff)
+//             }
+
+//             // if stream.write returns false, stop the loop
+//             if (!stream.write(buff)) break;
+
+//             i++;
+//         }
+//     }
+//     writeMany()
+
+//     // resume our loop once our stream's internal buffer is empty
+//     stream.on("drain", () => {
+//         writeMany()
+//     })
+
+//     stream.on("finish", ()=>{
+//         console.timeEnd("writeMany")
+//         fileHandler.close()
+//     })
+
+// })()
