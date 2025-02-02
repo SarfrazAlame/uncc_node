7487f5da7714792fcbbc48c14c64db63e4aadd08
Sarfraz Alam <sarfrazk7858@gmail.com>
2025-02-02 08:34:26 +0530

    route handle

---
+++ http/simple-web/server.js
@@ -0,0 +1,50 @@
+const http = require("node:http")
+const fs = require('node:fs/promises')
+
+const server = http.createServer();
+
+server.on("request", async (request, response) => {
+    if (request.url === "/" && request.method === 'GET') {
+        response.setHeader("Content-Type", "text/html")
+
+        const fileHandle = await fs.open("./public/index.html", "r")
+        const fileStream = fileHandle.createReadStream()
+
+        fileStream.pipe(response)
+    }
+
+    if (request.url === "/styles.css" && request.method === 'GET') {
+        response.setHeader("Content-Type", "text/css")
+
+        const fileHandle = await fs.open("./public/styles.css", "r")
+        const fileStream = fileHandle.createReadStream()
+
+        fileStream.pipe(response)
+
+    }
+
+    if (request.url === "/favicon.ico" && request.method === 'GET') {
+        response.setHeader("Content-Type", "text/favicon.ico")
+
+        const fileHandle = await fs.open("./public/favicon.ico", "r")
+        const fileStream = fileHandle.createReadStream()
+
+        fileStream.pipe(response)
+
+    }
+
+    if (request.url === "/scripts.js" && request.method === 'GET') {
+        response.setHeader("Content-Type", "text/javascript")
+
+        const fileHandle = await fs.open("./public/scripts.js", "r")
+        const fileStream = fileHandle.createReadStream()
+
+        fileStream.pipe(response)
+    }
+})
+
+
+server.listen(9000, () => {
+    console.log("Web server is live at http://localhost:9000")
+})
+
